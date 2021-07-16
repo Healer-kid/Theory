2c432f13f139dc58f067098ec68b3055189fc4e6
Aishiya Ganesan <69270775+Healer-kid@users.noreply.github.com>
2021-07-16 22:52:32 +0530

    Add files via upload

---
+++ OPERATING SYSTEM CODES/OS_Implementation of binary semaphores.cpp
@@ -0,0 +1,31 @@
+struct semaphore {
+	enum value(0, 1);
+
+	// q contains all Process Control Blocks (PCBs)
+	// corresponding to processes got blocked
+	// while performing down operation.
+	Queue<process> q;
+
+} P(semaphore s)
+{
+	if (s.value == 1) {
+		s.value = 0;
+	}
+	else {
+		// add the process to the waiting queue
+		q.push(P)
+		sleep();
+	}
+}
+V(Semaphore s)
+{
+	if (s.q is empty) {
+		s.value = 1;
+	}
+	else {
+
+		// select a process from waiting queue
+		Process p=q.pop();
+		wakeup(p);
+	}
+}
